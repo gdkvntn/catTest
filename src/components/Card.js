e46f0aa1a550f9c5d43abebecfc75f86367cce43
Anton <anton.gudkov1330@gmail.com>
2022-06-20 17:28:21 +0300

    1

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import CardActive from "./CardActive";
 
 const Header = styled.div`
   display: flex;
